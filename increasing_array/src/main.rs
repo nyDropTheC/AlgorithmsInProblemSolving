8f7f05c6820987e73b52ad81e1c54c07729dcfc0
nyDropTheC <125159296+nyDropTheC@users.noreply.github.com>
2023-05-23 14:37:13 +0300

    lol

--- increasing_array/src/main.rs
+++ increasing_array/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let n: i64 = input.trim ( ).parse ( ).expect ( "Input a valid number" );
+
     println!("Hello, world!");
 }
