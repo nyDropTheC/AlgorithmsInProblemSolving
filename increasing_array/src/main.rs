b7b23a3c60f82561f33dd1fb06d8fc4911c17803
nyDropTheC <125159296+nyDropTheC@users.noreply.github.com>
2023-05-23 15:14:58 +0300

    increasing

--- increasing_array/src/main.rs
+++ increasing_array/src/main.rs
@@ -1,13 +1,40 @@
 use std::io;
+use std::io::BufRead;
 
 fn main() {
     let mut input = String::new();
     
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+    io::stdin ( )
+        .read_line ( &mut input )
+        .expect ( "Failed to read line" );
 
-    let n: i64 = input.trim ( ).parse ( ).expect ( "Input a valid number" );
+    // literally useless, probably for the cniles doing this
 
-    println!("Hello, world!");
+    let mut list: Vec<i64> = io::stdin ( )
+        .lock ( )
+        .lines ( )
+        .next ( )
+        .unwrap ( )
+        .unwrap ( )
+        .trim ( )
+        .split_whitespace ( )
+        .map ( |s| s.parse::<i64> ( ).unwrap ( ) )
+        .collect::<Vec<i64>> ( );
+
+    // rust... uh...
+
+    let mut moves: i64 = 0;
+
+    for i in 1..list.len() {
+       let prev = list.get ( i - 1 ).unwrap ( );
+       let curr = list.get ( i ).unwrap ( );
+
+        if curr - prev < 0 {
+            let delta = prev - curr;
+            *&mut list[i] += delta;
+            moves += delta;
+        }
+    }
+
+    println!("{}", moves);
 }
